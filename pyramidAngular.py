92506da6ff8ee4fff2d238fbe8863438b37b55fe
Amit Kumar Karnik <kamit@dtl-kamit.(none)>
2016-05-20 17:52:46 +0530

    get project Euler's problems

--- pyramidAngular.py
+++ pyramidAngular.py
@@ -3,12 +3,38 @@ __author__ = "karnikamit"
 from pyramid.response import Response
 from pyramid.response import FileResponse
 import os
-
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+import json
 
 def home(request):
     path = os.path.abspath('templates/details.html')
     response = FileResponse(path)
     return response
+
+
+def problems(request):
+    path = os.path.abspath('templates/proj_euler.html')
+    resp = FileResponse(path)
+    return resp
+
+
+def get_page(request):
+    http = urllib3.PoolManager()
+    page = request.matchdict['ipage']
+    url = 'https://projecteuler.net/problem=' + str(page)
+    resp = http.request('GET', url)
+    data = {"response": "failure"}
+    if resp.status == 200:
+        soup = BeautifulSoup(resp.data, 'html.parser')
+        result_set = soup.find(role='problem').find_all('p')
+        res_list = []
+        for i in result_set:
+            res_list.append(i.text)
+        data['data'] = res_list
+        data['response'] = 'success'
+    return json.dumps(data)
 
 
 def hello_world(request):
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ __author__ = "karnikamit"
 import argparse
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from pyramidAngular import home
+import pyramidAngular
 
 
 if __name__ == '__main__':
@@ -15,8 +15,13 @@ if __name__ == '__main__':
         p = int(args.port)
     config = Configurator()
     config.add_route('home', '/')
-    config.add_view(home, route_name='home')
+    config.add_route('prob', '/projectEuler')
+    config.add_route('page', '/{ipage}')
+    config.add_view(pyramidAngular.home, route_name='home')
+    config.add_view(pyramidAngular.problems, route_name='prob')
+    config.add_view(pyramidAngular.get_page, route_name='page', renderer='json')
     app = config.make_wsgi_app()
-    print 'Running application on port %d' % p
-    server = make_server('localhost', p, app)
+    run = 'http://127.0.0.1:%d' % p
+    print 'Running application on: %s' % run
+    server = make_server('127.0.0.1', p, app)
     server.serve_forever()
